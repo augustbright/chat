5d6238e59b4daf31c67ae1c84e739bb99ad7f1e6
vtumanov782 <vtumanov782@gmail.com>
2019-11-07 16:35:06 +0300

    setup server application

--- server/start.js
+++ server/start.js
@@ -1,6 +1,49 @@
-import express from 'express';
-import next from 'next';
+import express from "express";
+import next from "next";
+import session from "express-session";
+import ConnectMongo from "connect-mongo";
+import { setupMongoClient } from "./database";
 
-export default module.exports = async ({NODE_ENV, PORT}) => {
-    return true;
+export default module.exports = async ({
+  NODE_ENV,
+  PORT,
+  MONGO_URL,
+  MONGO_DB,
+  COOKIE_SECRET
+}) => {
+  //Create Express app
+  const app = express();
+  app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
+  });
+
+  //Setup MongoDB client
+  const [mongoClient, mongoDB] = await setupMongoClient({
+    MONGO_URL,
+    MONGO_DB
+  });
+  app.set("mongoDB", mongoDB);
+
+  //Setup session
+  const MongoStore = ConnectMongo(session);
+  const mongoStore = new MongoStore({
+    autoReconnect: true,
+    client: mongoClient
+  });
+  const sessionMiddleware = session({
+    secret: COOKIE_SECRET,
+    rolling: true,
+    store: mongoStore
+  });
+  app.use(sessionMiddleware);
+
+  //Setup next.js
+  const nextApp = next({ dev: NODE_ENV === "development" });
+  const nextHandler = nextApp.getRequestHandler();
+  await nextApp.prepare();
+  app.get("*", (req, res) => {
+    nextHandler(req, res);
+  });
+
+  return true;
 };
